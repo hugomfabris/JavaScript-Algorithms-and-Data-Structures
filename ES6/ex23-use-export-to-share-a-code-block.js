dba080785eefe4a2d34bdd4d43a8ca9eba570dd0
Hugo Fabris <hugomfabris@gmail.com>
2021-06-09 17:54:55 -0300

    Fourth commit

---
+++ ES6/ex23-use-export-to-share-a-code-block.js
@@ -0,0 +1,11 @@
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export {uppercaseString}
+
+export {lowercaseString}
