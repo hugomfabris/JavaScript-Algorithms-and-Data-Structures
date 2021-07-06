5f9e8a0c2aedebf5f2cf787cc3e7691abaec94bf
Hugo Fabris <hugomfabris@gmail.com>
2021-07-06 15:32:02 -0300

    Functional Programming - First commit

---
+++ Functional Programming/ex04-avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,12 @@
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+
+  let newValue = fixedValue + 1;
+
+  return newValue
+
+  // Only change code above this line
+}
---
+++ Functional Programming/ex05-pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,11 @@
+// The global variable
+var fixedValue = 4;
+
+// Only change code below this line
+function incrementer (arg) {
+
+  let newValue = arg + 1;
+
+  return newValue 
+  // Only change code above this line
+}
