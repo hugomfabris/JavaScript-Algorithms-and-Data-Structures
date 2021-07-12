c4ef4182b880376bedc31340ad9a1aa58978cbac
Hugo Fabris <hugomfabris@gmail.com>
2021-07-12 19:35:08 -0300

    Functional Programming - Fifth commit

---
+++ Functional Programming/ex18-return-a-sorted-array-without-changing-the-original-array.js
@@ -0,0 +1,14 @@
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Only change code below this line
+
+  let newArr = [...arr]
+  newArr.sort(function(a, b) {
+    return a - b
+  })
+
+  return newArr
+  // Only change code above this line
+}
+
+console.log(nonMutatingSort(globalArray));
