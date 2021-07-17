f1fb9a438049f5bf88a179305556c299cf2f3fcd
Hugo Fabris <hugomfabris@gmail.com>
2021-07-17 17:01:24 -0300

    Functional Programming - Second commit

--- Functional Programming/ex18-return-a-sorted-array-without-changing-the-original-array.js
+++ Functional Programming/ex18-return-a-sorted-array-without-changing-the-original-array.js
@@ -1,6 +1,5 @@
 var globalArray = [5, 6, 3, 2, 9];
 function nonMutatingSort(arr) {
-  // Only change code below this line
 
   let newArr = [...arr]
   newArr.sort(function(a, b) {
@@ -8,7 +7,6 @@ function nonMutatingSort(arr) {
   })
 
   return newArr
-  // Only change code above this line
 }
 
 console.log(nonMutatingSort(globalArray));
--- Functional Programming/ex21-apply-functional-programming-to-convert-strings-to-url-slugs.js
+++ Functional Programming/ex21-apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -1,11 +1,8 @@
-// Only change code below this line
 function urlSlug(title) {
 
 let arr = title.split(' ').map(item => item.toLowerCase()).filter(item => item !== '').join('-')
 
 return arr
 }
-// Only change code above this line
-
 
 console.log(urlSlug(" Winter Is  Coming"))
