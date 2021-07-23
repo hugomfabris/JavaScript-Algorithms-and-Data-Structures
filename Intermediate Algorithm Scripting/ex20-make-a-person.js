cc8e18a9cb0e845dfe589a63252d51d0f82db95a
Hugo Fabris <hugomfabris@gmail.com>
2021-07-23 20:35:31 -0300

    Intermidiate Algorithm Scripting - Sixth commit

---
+++ Intermediate Algorithm Scripting/ex20-make-a-person.js
@@ -0,0 +1,30 @@
+var Person = function(firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  let fullName = firstAndLast
+  
+  this.getFullName = function() {
+    return fullName;
+  };
+  this.getFirstName = function() {
+    return fullName.split(' ')[0]
+  }
+  this.getLastName = function() {
+    return fullName.split(' ')[1]
+  }
+  this.setFirstName = function(first) {
+    fullName = first + ' ' + fullName.split(' ')[1] 
+  }
+  this.setLastName = function(last) {
+    fullName = fullName.split(' ')[0] + ' ' + last
+  }
+  this.setFullName = function(name) {
+    fullName = name
+  }
+};  
+ 
+var bob = new Person('Bob Ross');
+
+bob.setFirstName("Haskell")
+ 
+console.log(bob.getFullName())
