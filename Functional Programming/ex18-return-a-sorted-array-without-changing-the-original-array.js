a7542fe881fceb1042847d53c1a632e7389db3c6
Hugo Fabris <hugomfabris@gmail.com>
2021-07-24 17:27:27 -0300

    Functional Programming - Third commit

--- Functional Programming/ex18-return-a-sorted-array-without-changing-the-original-array.js
+++ Functional Programming/ex18-return-a-sorted-array-without-changing-the-original-array.js
@@ -5,7 +5,7 @@ function nonMutatingSort(arr) {
   newArr.sort(function(a, b) {
     return a - b
   })
-
+//For positive values, the callback function will change the order of the arguments
   return newArr
 }
 
